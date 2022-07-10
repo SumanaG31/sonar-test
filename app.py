a620bc5c2e1da402cf1d71d08cf6776fe5e57030
Sumana <gandesumana@gmail.com>
2022-07-10 20:33:46 +0530

    noncompliant code

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import re
+
+init = "Bob is a Bird... Bob is a Plane... Bob is Superman!"
+changed = re.sub(r"Bob is", "It's", init) # Noncompliant
+changed = re.sub(r"\.\.\.", ";", changed) # Noncompliant
+
 print('Hellooo World !!') 
