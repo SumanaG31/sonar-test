0b82951e7be9c24bd280f4c1f38dc182446ff29c
Sumana <gandesumana@gmail.com>
2022-07-18 17:16:29 +0530

    bug

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print('Welcome to SonarQube Demo Project !') 
+
+def func(a, b, c):
+    return a * b * c
+
+func(6, 93, 31, c=62) # Noncompliant: argument "c" is duplicated
+
+params = {'c':31}
+func(6, 93, 31, **params) # Noncompliant: argument "c" is duplicated
+func(6, 93, c=62, **params) # Noncompliant: argument "c" is duplicated
